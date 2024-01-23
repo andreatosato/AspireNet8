78bf7372a68ba7b4885965f670e36aa4c515e4f7
Andrea Tosato <andrea.tosato@beta80group.it>
2024-01-23 11:49:04 +0100

    DAPR Work

---
+++ srcDAPR/AspireWithDapr.AppHost/Program.cs
@@ -0,0 +1,9 @@
+﻿var builder = DistributedApplication.CreateBuilder(args);
+
+builder.AddProject<Projects.AspireWithDapr_ApiService>("apiservice")
+       .WithDaprSidecar("api");
+
+builder.AddProject<Projects.AspireWithDapr_Web>("webfrontend")
+       .WithDaprSidecar("web");
+
+builder.Build().Run();
